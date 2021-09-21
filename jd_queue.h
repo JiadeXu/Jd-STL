36d04d5806d70eec4c7dd369d328f066480a0f22
JiadeXu <1621396712@qq.com>
2021-09-21 23:23:46 +0800

    队列
    队列 配接器

--- jd_queue.h
+++ jd_queue.h
@@ -7,4 +7,46 @@
 
 #ifndef _JD_QUEUE_H
 #define _JD_QUEUE_H
+
+#include "jd_macro.h"
+#include "jd_list.h"
+
+JD_SPACE_BEGIN
+
+template<class T, class Sequence = JD::list<T> >
+class queue {
+	template<class T2, class Sequence2 >
+	friend bool operator== (const queue<T2, Sequence2> &, const queue<T2, Sequence2> &);
+	template<class T2, class Sequence2 >
+	friend bool operator< (const queue<T2, Sequence2> &, const queue<T2, Sequence2> &);
+public:
+	typedef typename Sequence::value_type value_type;
+	typedef typename Sequence::size_type size_type;
+	typedef typename Sequence::reference reference;
+	typedef typename Sequence::const_reference const_reference;
+protected:
+	// Sequence c; // 底层容器
+	Sequence c;
+public:
+	bool empty() { return c.empty(); }
+	size_type size() { return c.size(); }
+	reference front() { return c.front(); }
+	const_reference front() const { return c.front(); }
+	// queue是先进后出 即 末端进末端出
+	void push(const value_type &x) { c.push_back(x); }
+	void pop() { c.pop_front(); }
+};
+
+template<class T, class Sequence>
+bool operator==(const JD::queue<T, Sequence> &x, const JD::queue<T, Sequence> &y) {
+	return x.c == y.c;
+}
+
+template<class T, class Sequence>
+bool operator< (const JD::queue<T, Sequence> &x, const JD::queue<T, Sequence> &y) {
+	return x.c < y.c;
+}
+
+JD_SPACE_END
+
 #endif
--- test_programs/04.stack_queue.cpp
+++ test_programs/04.stack_queue.cpp
@@ -7,6 +7,8 @@
 
 #include<iostream>
 #include "../jd_stack.h"
+#include "../jd_queue.h"
+#include <algorithm>
 using namespace std;
 
 // using namespace std;
@@ -23,5 +25,18 @@ int main() {
 		std::cout << iStack.top() << std::endl;
 		iStack.pop();
 	}
+	std::cout << "size " << iStack.size() << std::endl;
+
+	JD::queue<int> que;
+	for (int i = 0; i < 5; i++) {
+		que.push(2 * i + 1);
+	}
+
+	std::cout << "size " << que.size() << std::endl;
+	while(!que.empty()) {
+		std::cout << que.front() << std::endl;
+		que.pop();
+	}
+	std::cout << "size " << que.size() << std::endl;
 	return 0;
 }
