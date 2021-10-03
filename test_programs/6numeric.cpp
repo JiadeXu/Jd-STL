9bf3be89dd5b4c2c804e8604f56e1c5a9ac5f28f
JiadeXu <1621396712@qq.com>
2021-10-03 16:39:54 +0800

    使用 jd_numeric.h

--- test_programs/6numeric.cpp
+++ test_programs/6numeric.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <numeric>
 #include "../jd_vector.h"
+#include "../jd_numeric.h"
 #include <utility>
 #include <vector>
 #include <functional>
@@ -21,29 +22,31 @@ int main() {
 		iv.push_back(ia[i]);
 	}
 
-	cout << accumulate(iv.begin(), iv.end(), 0) << endl;
-	cout << accumulate(iv.begin(), iv.end(), 0, minus<int>()) << endl;
+	cout << JD::accumulate(iv.begin(), iv.end(), 0) << endl;
+	cout << JD::accumulate(iv.begin(), iv.end(), 0, minus<int>()) << endl;
 
 	// 10 + 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5
-	cout << inner_product(iv.begin(), iv.end(), iv.begin(), 10) << endl;
+	cout << JD::inner_product(iv.begin(), iv.end(), iv.begin(), iv.end(), 10) << endl;
 		// 10 + 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5
-	cout << inner_product(iv.begin(), iv.end(), iv.begin(), 10, minus<int>(), plus<int>()) << endl;
+	cout << JD::inner_product(iv.begin(), iv.end(), iv.begin(), iv.end(), 10, minus<int>(), plus<int>()) << endl;
 
 	ostream_iterator<int> oite(cout, " ");
-	partial_sum(iv.begin(), iv.end(), oite); // 1 3 6 10 15
+	JD::partial_sum(iv.begin(), iv.end(), oite); // 1 3 6 10 15
 	cout << endl;
-	partial_sum(iv.begin(), iv.end(), oite, minus<int>()); // 1 -1 -4 -8 -13
+	JD::partial_sum(iv.begin(), iv.end(), oite, minus<int>()); // 1 -1 -4 -8 -13
 	cout << endl;
 
-	adjacent_difference(iv.begin(), iv.end(), oite);
+	JD::adjacent_difference(iv.begin(), iv.end(), oite);
 
-	adjacent_difference(iv.begin(), iv.end(), oite, plus<int>());
+	JD::adjacent_difference(iv.begin(), iv.end(), oite, plus<int>());
+	cout << endl;
 
-	// cout << std::pow(10, 3) << endl;
-	// cout << std::pow(10, 3) << endl;
+	cout << JD::power(10, 3) << endl;
+	cout << JD::power(10, 3, std::plus<int>()) << endl; // 10 + 10 + 10
 
 	int n = 3;
-	iota(iv.begin(), iv.end(), n);
+	cout << "iota ";
+	JD::iota(iv.begin(), iv.end(), n);
 	for (int i = 0; i < iv.size(); i++) {
 		cout << "iv[" << i << "] " << iv[i] << " ";
 	}
