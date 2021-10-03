950a4acdcf99133e04373a350450a5aca523a859
JiadeXu <1621396712@qq.com>
2021-10-03 23:14:37 +0800

    4个set算法

--- jd_algobase.h
+++ jd_algobase.h
@@ -334,6 +334,85 @@ inline BidirectionalIterator2 copy_backward(BidirectionalIterator1 first, Bidire
 }
 
 #endif
+
+// set_union
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+OutputIterator set_union(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
+	J_LOG();
+	while (first1 != last1 && first2 != last2) {
+		if (*first1 < *first2) {
+			*result = *first1;
+			++first1;
+		} else if (*first2 < *first1) {
+			*result = *first2;
+			++first2;
+		} else {
+			*result = *first1;
+			++first1;
+			++first2;
+		}
+		++result;
+	}
+	// 拼接上剩余部分
+	return JD::copy(first2, last2, JD::copy(first1, last1, result));
+}
+
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+OutputIterator set_intersection(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
+	J_LOG();
+	while(first1 != last1 && first2 != last2) {
+		if (*first1 < *first2) {
+			++first1;
+		} else if (*first2 < *first1) {
+			++first2;
+		} else {
+			*result = *first1;
+			++first1;
+			++first2;
+			++result;
+		}
+	}
+	return result;
+}
+
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+OutputIterator set_difference(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
+	J_LOG();
+	while(first1 != last1 && first2 != last2) {
+		if (*first1 < *first2) {
+			*result = *first1;
+			++first1;
+			++result;
+		} else if (*first2 < *first1) {
+			++first2;
+		} else {
+			++first1;
+			++first2;
+		}
+	}
+	return JD::copy(first1, last1, result);
+}
+
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+OutputIterator set_symmetric_difference(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, OutputIterator result) {
+	J_LOG();
+	while(first1 != last1 && first2 != last2) {
+		if (*first1 < *first2) {
+			*result = *first1;
+			++first1;
+			++result;
+		} else if (*first2 < *first1) {
+			*result = *first2;
+			++first2;
+			++result;
+		} else {
+			++first2;
+			++first1;
+		}
+	}
+	return JD::copy(first2, last2, JD::copy(first1, last1, result));
+}
+
 JD_SPACE_END
 
 #endif
